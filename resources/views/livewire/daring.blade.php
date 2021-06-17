f734a1482d561f97969559197fa43556a1838ee7
fadilAndrian <akhsanfadil@gmail.com>
2021-06-17 13:06:48 +0700

    maintenance on daring-page

--- resources/views/livewire/daring.blade.php
+++ resources/views/livewire/daring.blade.php
@@ -1,3 +1,16 @@
 <div>
-    @include('livewire.datetime_picker')
+
+  <div class="flex flex-col">
+    <!-- This example requires Tailwind CSS v2.0+ -->
+	<div class="bg-gray-50">
+	  <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+	    <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+	      <span class="block">Maaf, balik lagi nanti ya 👷</span>
+	      <span class="block text-indigo-600">Fitur ini masih dikembangkan sama developernya hehe</span>
+	    </h2>
+	  </div>
+	</div>
+
+  </div>
+
 </div>
